7ea4004420fba4d9a58a9cac22668cc23270cd97
crlotwhite <crlotwhite@gmail.com>
2024-10-10 02:44:59 +0900

    Fix causal padding logic

--- models/causal_conv_1d.py
+++ models/causal_conv_1d.py
@@ -1,12 +1,15 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class CausalConv1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, **kwargs):
         super(CausalConv1d, self).__init__()
+        self.kernel_size = kernel_size
+        self.dilation = dilation
         self.padding = (kernel_size - 1) * dilation
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size,
-                              dilation=dilation, padding=self.padding)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, dilation=dilation, **kwargs)
 
     def forward(self, x):
-        out = self.conv(x)
-        return out[:, :, :-self.padding]  # Remove padding
+        # 오른쪽으로 패딩을 추가하여 Causal 패딩 적용
+        x = F.pad(x, (self.padding, 0))
+        return self.conv(x)
